d08d9fe1ef1c263ff0c9cee01f3c9202ee413460
benderang26 <benderang26@gmail.com>
2023-05-27 19:25:30 +0800

    add useEffect and UseState and fetch API from server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './api/axiosConfig'
+import Layout from "./components/Layout";
+import { Routes, Route} from 'react-router-dom'
 
 function App() {
+  const [movies, setMovies] = useState()
+
+  const getMoviesFromAPI = async () => {
+    try {
+    const response = await api.get('api/v1/movies')
+    console.log(response.data)
+
+    setMovies(response.data)
+
+
+    } catch(err) {
+      console.log(err)
+    }
+  }
+  useEffect(() => {
+    getMoviesFromAPI()
+  })
+
   return (
     <div>
-
+      <Routes>
+        <Route path ='/' element={Layout} />
+        <Route></Route>
+      </Routes>
     </div>
   );
 }
